e8009d21d8331c54c58244778bfb74a2b4b1c2b7
Ronald Wildenberg <rwildenberg@itq.nl>
2018-01-16 22:53:26 +0100

    Added and configured logging to WorldFetcherHostedService

--- Lib/WorldFetcherHostedService.cs
+++ Lib/WorldFetcherHostedService.cs
@@ -5,6 +5,7 @@ namespace worldfetch.Lib
     using System.Net.Http;
     using System.Threading;
     using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Options;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
@@ -15,10 +16,13 @@ namespace worldfetch.Lib
         private static HttpClient HttpClient = new HttpClient();
 
         private readonly IOptions<FetcherOptions> dataOptionsAccessor;
+        private readonly ILogger<WorldFetcherHostedService> logger;
 
-        public WorldFetcherHostedService(IOptions<FetcherOptions> dataOptionsAccessor)
+        public WorldFetcherHostedService(
+            IOptions<FetcherOptions> dataOptionsAccessor, ILogger<WorldFetcherHostedService> logger)
         {
             this.dataOptionsAccessor = dataOptionsAccessor;
+            this.logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken ct)
@@ -37,7 +41,9 @@ namespace worldfetch.Lib
                     {
                         dataUriBuilder.Query = $"output=json&rows=200&offset={offset}";
                         return dataUriBuilder.Uri;
-                    });
+                    })
+                    .ToList();
+                logger.LogDebug("Fetching from {n} data uri's: {dataUris}", dataUris.Count, string.Join(" | ", dataUris));
 
                 // Run paged fetches in parallel.
                 var fetchTasks = dataUris
